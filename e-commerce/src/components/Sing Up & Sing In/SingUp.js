87d50291529dbe4c2364fdb7b311feb5ef7ae72b
UygarKAYA <uygar.kaya@ozu.edu.tr>
2021-08-04 18:28:49 +0300

    Post Request is created with using axios

--- e-commerce/src/components/Sing Up & Sing In/SingUp.js
+++ e-commerce/src/components/Sing Up & Sing In/SingUp.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import axios from 'axios'
 import '../../App.css'
 
 function SingUp() {
+
+    const SingUpToMongoDB = event => {
+        let name = event.target[0].value;
+        let surname = event.target[1].value;
+        let email = event.target[2].value;
+        let username = event.target[3].value;
+        let password = event.target[4].value;
+
+        let SingUpData = {
+            name, surname, email,
+                username, password
+        }
+
+        postSingUpToMongoDB(SingUpData);
+    }
+
+    const postSingUpToMongoDB = SingUpData => {
+        axios.post('http://localhost:8080/api/users/addUser', SingUpData)
+                .then(response => { alert("User Successfully Created"); })
+                    .catch(error => alert(error));
+    }
+
     return (
         <div className="sing-up">
             <div className="container">
                 <div className="sign-up-h1">
                     <h1><i>Create Account</i></h1>
                 </div>
-                <form>
+                <form onSubmit={event => {event.preventDefault(); SingUpToMongoDB(event);}}>
                     <label><i>Name</i></label>
                     <input type="text" className="form-control" placeholder="Name" required></input>
                     <label><i>Surname</i></label>
